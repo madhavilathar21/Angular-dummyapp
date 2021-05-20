bb7038a325f574911e53544df38a5340b9266fe0
madhavilathar21 <38210429+madhavilathar21@users.noreply.github.com>
2021-05-20 23:04:44 +0530

    Add files via upload

---
+++ login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isSubmitted = false;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get formControls() { return this.loginForm.controls; }
+
+  login() {
+    console.log(this.loginForm.value);
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    // this.authService.login(this.loginForm.value);
+    this.router.navigateByUrl('/regionWise');
+  }
+
+}
